b546f188733b482ffc2d782e9ec29aa17296c268
Tadas <transterus8@GMAIL.COM>
2024-09-02 20:13:20 +0300

    Prideta skaiciavimo logika

--- src/main/java/com/spring/pradzia/Internetinis/Skaiciuotuvas/InternetinisSkaiciuotuvasController.java
+++ src/main/java/com/spring/pradzia/Internetinis/Skaiciuotuvas/InternetinisSkaiciuotuvasController.java
@@ -4,6 +4,8 @@ package com.spring.pradzia.Internetinis.Skaiciuotuvas;
 import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -12,55 +14,73 @@ import java.util.HashMap;
 //@RestController anotacija nurodo, jog pvz. String tipo rezultatas turetu buti ispaudziamas kientui toks koks yra
 
 
-@RestController
+//@Restcontroller anotacija naudojama tada kai frontende nenaudojam springoJavaScript, React, Angular)
+//Dazniausiai grazinami formatai json, xml
+//Tai yra negraziname vaizdo(formos HTML JSP
+// kadangi mums reikia grazinti vaizda(view) pagal Spring mvc, naudosime anotacija @Controller
+
+@Controller
 //Zymi konfiguracijos komponeneta. Viduje leidzia kurti Bean per metodus su@Bean anotacija
 //Si klases lygio anotacija nurodo Spring karkasui "Atspeti" konfiguracija.
 //Remiantis priklausomybemis (Jar bibliotekomis), kurias programuotojas itraukia i projekta (pom.xml)
 // Siuo atveju ji veikia kartu su main metodu.
 
 @EnableAutoConfiguration
-
-
 public class InternetinisSkaiciuotuvasController {
-    @RequestMapping(method = RequestMethod.GET, value = "/skaiciuoti")
 
-//RequestParam anotacija perduoda siuo atveju per url perduotus duomenis (du skaiciai operacijos zenklas)
+    //Kadangi skaiciuotuvo forma naudoja POST metoda, cia irgi nurodysim POST
+    //@RequestMapping(method = RequestMethod.POST, value = "/skaiciuoti")
+@PostMapping("/skaiciuoti")
+    //RequestParam anotacija perduoda siuo atveju per url perduotus duomenis (du ivedimoSarasa operacijos zenklas)
     // kurie patalpinasi i sarasa (raktas reiksme)
     // 1 stringas yra raktas (sk1,sk2 zenklas), 2 stringas yra reiksme.
     // Raktai tiek frontende, tiek backende turi sutapti!!!
     //URL pavyzdys: http://127.0.0.1:8080/skaiciuoti?sk1=2&sk2=3&zenklas=*
 
-    public String Skaiciuoti(@RequestParam HashMap <String, String> skaiciai) {
+
+
+    public String Skaiciuoti(@RequestParam HashMap <String, String> ivedimoSarasa, ModelMap isvedimoSarasas) {
 
         //Per url perduodamas raktas (kintamasis) turi pavadinima sk1
         //Pagal rakta sk1 istraukiama reiksme pvz (tarkime vartotojas ivede 8)
         //Vadinasi mums reikia konvertuoti is String i Int kad paskaiciuotume rezultata
 
-        int sk1 =Integer.parseInt(skaiciai.get("sk1"));
-        int sk2 =Integer.parseInt(skaiciai.get("sk2"));
-        int rezultatas =0;
-        String zenklas = skaiciai.get("zenklas");
+        int sk1 = Integer.parseInt(ivedimoSarasa.get("sk1"));
+        int sk2 = Integer.parseInt(ivedimoSarasa.get("sk2"));
+        int rezultatas = 0;
+        String zenklas = ivedimoSarasa.get("zenklas");
 
         if (zenklas.equals("+")) {
             rezultatas = sk1 + sk2;
-        }else if (zenklas.equals("-")) {
+        } else if (zenklas.equals("-")) {
             rezultatas = sk1 - sk2;
-        }else if (zenklas.equals("*")) {
+        } else if (zenklas.equals("*")) {
             rezultatas = sk1 * sk2;
-        }else if (zenklas.equals("/")){
+        } else if (zenklas.equals("/")) {
             rezultatas = sk1 / sk2;
-       }
+        }
 
-        //N.D. Suskaiciuoti ir atspausdinti rezultata
-        return sk1 + " " + zenklas + " " + sk2 + " " + " = " + rezultatas;
+        //Ivedimo sarasas naudojamas siusti duomenis is Spring MVC controllerio i Jsp faila (vaizda)
+        isvedimoSarasas.put("sk1", sk1);
+        isvedimoSarasas.put("sk2", sk2);
+        isvedimoSarasas.put("zenklas", zenklas);
+        isvedimoSarasas.put("rezultatas", rezultatas);
+
+        //Grazinamas vaizdas(forma)
+        //Svarbu nurodyti per Aplication.properties prefix ir suffix
+        //Nes pagal tai ieskos vaizdo projekte
+
+        return "skaiciuoti";
+
+        //return sk1 + " " + zenklas + " " + sk2 + " " + " = " + rezultatas;
 
         //  ApplicationContext yra interfeisas skirtas suteikti informacija apie aplikacijos konfiguracija.
         // Siuo atveju naudojama konfiguracija paimama is beans.xml failo
         //ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
         //bean - klases objektas kuris atitinka singleton sablona.
         //HelloWorld bean = (HelloWorld) applicationContext.getBean("helloWorld");
-            //return bean.getHello();
-
+        //return bean.getHello();
+    }
 //        return"Internetinis skaiciuotuvas <p>" +
 //                "Galimos operacijos: <br>" +
 //                "&nbsp;&nbsp; sudeti <br>" +
@@ -68,6 +88,10 @@ public class InternetinisSkaiciuotuvasController {
 //                "&nbsp;&nbsp; sudauginti <br>" +
 //                "&nbsp;&nbsp; padalinti <br></p>";
 
+    @RequestMapping(method = RequestMethod.GET, value =  "/")
+    public String rodytiPagrindiniPuslapi(){
+        //Graziname jsp faila, kuris turi buti talpinamas "webapp--> WEB-INF -->JSP" aplanke
+        return "skaiciuotuvas";
     }
 }
 
